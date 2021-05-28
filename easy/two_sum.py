4c6c295220850515b753c6d132fe7b40dad1c3ba
Jinho Lee <jl5027@columbia.edu>
2021-05-28 23:25:38 +0900

    Update two_sum.py

--- easy/two_sum.py
+++ easy/two_sum.py
@@ -2,8 +2,7 @@ class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         indices = list()
         
-        
-        
+        # O(n^2) > 
         
         return indices
                 
