6b8cee62ff57077f089b01015e3cdb57a00bdd0d
Jinho Lee <jl5027@columbia.edu>
2021-05-29 21:41:02 +0900

    Update reverse_integer.py

--- easy/reverse_integer.py
+++ easy/reverse_integer.py
@@ -1,3 +1,24 @@
 class Solution:
     def reverse(self, x: int) -> int:
+        digits = list(str(x))
+        digits = digits[::-1]
         
+        # edge case #1 (If the given number is negative, the result shouldn't reverse the sign.)
+        if x < 0:
+            temp = digits[len(digits) - 1]
+            digits.pop()
+            digits.insert(0, temp)
+                
+        digit = str()
+        
+        for i in range(len(digits)):
+            digit += digits[i]
+        
+        digit = int(digit)
+        
+        # edge case #2 (The digit must be bigger than -2^31 or smaller than 2^31 - 1.)
+        if digit < (-2)**31 or digit > (2**31 - 1):
+            digit = 0
+            
+            
+        return digit
