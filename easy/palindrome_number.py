b8455ba1dd952a935da2c47886a7bd64fc7281ae
Jinho Lee <jl5027@columbia.edu>
2021-05-29 23:40:38 +0900

    Update palindrome_number.py

--- easy/palindrome_number.py
+++ easy/palindrome_number.py
@@ -2,9 +2,12 @@ class Solution:
     def isPalindrome(self, x: int) -> bool:
         # Advanced solution without converting the integer into a string.
         # Runtime: 108 ms, Memory usage: 14.4 MB
+        
+        # Case #1: Negative numbers cannot be pallindromes.
         if x < 0:
             return False
         
+        # Case #2: Positive single digit numbers are all pallindromes.
         elif x >= 0 and x <= 9: 
             return True
         
@@ -23,6 +26,7 @@ class Solution:
         # Solution #2 with converting the integer into a string.
         # Runtime: 76 ms, Memory usage: 14.2 MB
         """
+        # Easy list apprehension
         xlist = [str(i) for i in str(x)]
         ylist = [str(i) for i in str(x)]
         xlist.reverse()
@@ -31,4 +35,3 @@ class Solution:
         else:
             return False
         """
-        
