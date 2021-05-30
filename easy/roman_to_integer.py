29aedf737853ee4b91c131cbdfd37f852918a688
Jinho Lee <jl5027@columbia.edu>
2021-05-30 20:57:28 +0900

    Update roman_to_integer.py

--- easy/roman_to_integer.py
+++ easy/roman_to_integer.py
@@ -1,3 +1,24 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
+        res = 0
+        roman = [str(i) for i in str(s)]
+        switch = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000
+        }
         
+        for i in range(len(roman) - 1):
+            if switch[roman[i]] < switch[roman[i + 1]]:
+                if switch[roman[i]] == 1:
+                    res -= 1
+                
+                if switch[roman[i]] == 10:
+                    res -= 10
+                
+                if switch[roman[i]] == 100:
+                    res -= 100
