868c1d3e8a89a71f89d765527ef86e40f57f3bb8
Jinho Lee <jl5027@columbia.edu>
2021-06-03 00:34:47 +0900

    Update search_insert.py

--- easy/search_insert.py
+++ easy/search_insert.py
@@ -1,4 +1,5 @@
 # Runtime: 44 ms, Memory usage: 14.9 MB
+# Only allowed to implement a function with O(n log n) runtime complexity
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
         if target in nums:
