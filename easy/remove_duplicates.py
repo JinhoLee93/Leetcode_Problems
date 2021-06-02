6192c2e167c8e993ce4140d874d8c5d034be2103
Jinho Lee <jl5027@columbia.edu>
2021-06-02 20:49:03 +0900

    Update remove_duplicates.py

--- easy/remove_duplicates.py
+++ easy/remove_duplicates.py
@@ -1,3 +1,15 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
+        i = 0
+        while True: 
+            try: 
+                if nums[i] == nums[i + 1]: 
+                    nums.pop(i)
+                    
+                else:
+                    i += 1
+                
+            except: 
+                break
         
+        return len(nums)
