5a0468b1dd910cdb96e07bfcad2425876b960aa5
Jinho Lee <jl5027@columbia.edu>
2021-06-02 21:03:36 +0900

    Update remove_element.py

--- easy/remove_element.py
+++ easy/remove_element.py
@@ -1,3 +1,12 @@
+# runtime: 24 ms, Memory usage: 13.9 MB
+# Only allowed to implement O(1) extra memory
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
-        
+        i = 0
+        while i < len(nums): 
+            if nums[i] == val:
+                nums.pop(i)
+            else:
+                i += 1
+
+        return len(nums)
