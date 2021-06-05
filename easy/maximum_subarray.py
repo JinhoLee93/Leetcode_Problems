d06031e5c17d2427a5c92ec91e5b42c9ad6c547f
Jinho Lee <jl5027@columbia.edu>
2021-06-05 20:31:59 +0900

    Update maximum_subarray.py

--- easy/maximum_subarray.py
+++ easy/maximum_subarray.py
@@ -1,7 +1,9 @@
 # Using Divide and Conquer 
 class Solution:
     def conquer(self, nums, index, l):
+        max = -1
         
+        if 
         
     def maxSubArray(self, nums: List[int]) -> int:
         max = -float(inf)
