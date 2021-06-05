1107e2e8ce19755c51bbc371f765686818723c76
Jinho Lee <jl5027@columbia.edu>
2021-06-06 00:03:47 +0900

    Update maximum_subarray.py

--- easy/maximum_subarray.py
+++ easy/maximum_subarray.py
@@ -1,23 +1,12 @@
 # Runtime: 76 ms, Memory usage: 28.9 MB
+# O(n)
 class Solution:
-    def conquer(self, m, res, nums, index):
-        if index == len(nums):
-            return res
-        
-        else:
-            if m + nums[index] < nums[index]:
-                if nums[index] > res:
-                    res = nums[index]
-                    
-                return self.conquer(nums[index], res, nums, index + 1) 
-            
-            else: 
-                if m + nums[index] > res:
-                    res = m + nums[index]
-                
-                return self.conquer(m + nums[index], res, nums, index + 1)
-
     def maxSubArray(self, nums: List[int]) -> int:
-        res = self.conquer(-float('inf'), -float('inf'), nums, 0)
+        max = -float('inf')
+        start = 0
+        end = len(nums) - 1
         
-        return res
+        for i in range(len(nums)): 
+            if max + nums[i] > max:
+                start = 0
+                end = i
