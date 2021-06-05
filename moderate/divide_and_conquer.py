a91c344e394f82e4fa590ddaf914a21446173a20
Jinho Lee <jl5027@columbia.edu>
2021-06-05 23:49:11 +0900

    Update divide_and_conquer.py

--- moderate/divide_and_conquer.py
+++ moderate/divide_and_conquer.py
@@ -1,17 +1,23 @@
+# Runtime: 76 ms, Memory usage: 28.9 MB
 class Solution:
-    def conquer(self, m, nums, index):
+    def conquer(self, m, res, nums, index):
         if index == len(nums):
-            return m
+            return res
         
         else:
-            if 
             if m + nums[index] < nums[index]:
-                return self.conquer(nums[index], nums, index + 1) 
+                if nums[index] > res:
+                    res = nums[index]
+                    
+                return self.conquer(nums[index], res, nums, index + 1) 
             
-            elif m + nums[index] >:
-                return self.conquer(m + nums[index], nums, index + 1)
+            else: 
+                if m + nums[index] > res:
+                    res = m + nums[index]
+                
+                return self.conquer(m + nums[index], res, nums, index + 1)
 
     def maxSubArray(self, nums: List[int]) -> int:
-        m = self.conquer(-float('inf'), nums, 0)
+        res = self.conquer(-float('inf'), -float('inf'), nums, 0)
         
-        return m
+        return res
