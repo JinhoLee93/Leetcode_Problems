9d222f1def53729544889080c281835316af5ecf
Jinho Lee <jl5027@columbia.edu>
2021-06-06 20:49:32 +0900

    Update length_of_last_word.py

--- easy/length_of_last_word.py
+++ easy/length_of_last_word.py
@@ -1,3 +1,13 @@
+# Runtime: 32 ms, Memory usage: 14.2 MB
 class Solution:
     def lengthOfLastWord(self, s: str) -> int:
+        words = s.split(" ")
         
+        # Solving pesky edge cases
+        while True: 
+            if words[len(words) - 1] == '' and len(words) > 1:
+                words.pop()
+            else:
+                break
+                
+        return len(words[len(words) - 1])
