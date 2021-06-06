965017ad06baf304423e3110a6a210403a7e5242
Jinho Lee <jl5027@columbia.edu>
2021-06-06 23:59:13 +0900

    Update add_binary.py

--- easy/add_binary.py
+++ easy/add_binary.py
@@ -1,3 +1,9 @@
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
+        a = int(a)
+        b = int(b)
         
+        ab = [str(i) for i in str(a+b)]
+        
+        while True:
+            
