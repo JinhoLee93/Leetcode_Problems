43062fd44f9b29704593fe22584bf0629293909b
Jinho Lee <jl5027@columbia.edu>
2021-06-06 21:19:06 +0900

    Update plus_one.py

--- easy/plus_one.py
+++ easy/plus_one.py
@@ -1,3 +1,12 @@
+# Runtime: 42 ms, Memory usage: 14 MB
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
+        num = 0
+        for i in range(len(digits)):
+            num += digits[i] * (10 ** (len(digits) - 1 - i)) 
         
+        num += 1
+        
+        nums = [int(str(i)) for i in str(num)]
+        
+        return nums
