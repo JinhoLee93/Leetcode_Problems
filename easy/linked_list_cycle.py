f082ef363df8f5e11866b245def07cad9fcbb007
Jinho Lee <jl5027@columbia.edu>
2021-06-07 23:38:50 +0900

    Update linked_list_cycle.py

--- easy/linked_list_cycle.py
+++ easy/linked_list_cycle.py
@@ -1,10 +1,18 @@
 # Definition for singly-linked list.
- class ListNode:
-    def __init__(self, x):
-        self.val = x
-        self.next = None
+#class ListNode:
+#    def __init__(self, x):
+#        self.val = x
+#        self.next = None
 
 class Solution:
     def hasCycle(self, head: ListNode) -> bool:
-        
-        
+        seen = list()
+        while head is not None:
+            if head.val not in seen:
+                seen.append(head.val)
+                head = head.next
+            else: 
+                return True
+            
+        return False
+                
