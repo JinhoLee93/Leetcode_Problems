67de44d410312e11d3c221ca61cebdee8ee0e1f0
Jinho Lee <jl5027@columbia.edu>
2021-06-13 16:40:31 +0900

    Update linked_list_components.py

--- moderate/linked_list_components.py
+++ moderate/linked_list_components.py
@@ -1,1 +1,14 @@
-
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+def check(head, nums):
+    traverse = head
+    while traverse: 
+        print(traverse.val)
+        traverse = traverse.next
+    
+class Solution:
+    def numComponents(self, head: ListNode, nums: List[int]) -> int:
+        check(head, nums)
