8da9067437db9552db5f8d57cc19e8438cfc424c
Jinho Lee <jl5027@columbia.edu>
2021-06-13 16:17:12 +0900

    Update design_linked_list.py

--- moderate/design_linked_list.py
+++ moderate/design_linked_list.py
@@ -1,7 +1,8 @@
 class Node:
-    def __init__(self, val, self.next=None):
+
+    def __init__(self, val, next_node=None):
         self.val = val
-        self.next = next
+        self.next_node = next_node
 
 class MyLinkedList:
 
@@ -10,7 +11,6 @@ class MyLinkedList:
         Initialize your data structure here.
         """
         self.head = Node(None)
-        
 
     def get(self, index: int) -> int:
         """
@@ -21,23 +21,24 @@ class MyLinkedList:
         while traverse:
             if i == index:
                 return traverse.val
+
             i += 1
             traverse = traverse.next_node
 
         return -1
-        
 
-        def addAtHead(self, val: int) -> None:
+    def addAtHead(self, val: int) -> None:
         """
         Add a node of value val before the first element of the linked list. After the insertion, the new node will be the first node of the linked list.
         """
-        if not self.head.val:
+        if self.head.val == None:
             self.head.val = val
         else:
             traverse_old = self.head
             new_list = MyLinkedList()
             new_list.head.val = val
             traverse_new = new_list.head
+
             while traverse_old:
                 traverse_new.next_node = traverse_old
                 traverse_old = traverse_old.next_node
@@ -49,16 +50,14 @@ class MyLinkedList:
         """
         Append a node of value val to the last element of the linked list.
         """
-        new_node = Node(val)
-        if not self.head.val:
+        newNode = Node(val)
+        if self.head.val == None:
             self.head.val = val
         else:
             traverse = self.head
             while traverse.next_node:
                 traverse = traverse.next_node
-                
-            traverse.next_node = new_node
-        
+            traverse.next_node = newNode
 
     def addAtIndex(self, index: int, val: int) -> None:
         """
@@ -68,13 +67,14 @@ class MyLinkedList:
         traverse_old = self.head
         new_list = MyLinkedList()
         traverse_new = new_list.head
-            
+
         i = 0
         while traverse_old:
             if index == i:
                 traverse_new.val = new_node.val
-                traverse_new.next_node = Node(None)
-                traverse_new = traverse_new.next_node
+                if traverse_old:
+                    traverse_new.next_node = Node(None)
+                    traverse_new = traverse_new.next_node
             else:
                 traverse_new.val = traverse_old.val
                 traverse_old = traverse_old.next_node
@@ -83,6 +83,8 @@ class MyLinkedList:
                     traverse_new = traverse_new.next_node
             i += 1
 
+        if index == i:
+            new_list.addAtTail(val)
 
         self.head = new_list.head
 
@@ -90,14 +92,18 @@ class MyLinkedList:
         """
         Delete the index-th node in the linked list, if the index is valid.
         """
-            def deleteAtIndex(self, index: int) -> None:
-        """
-        Delete the index-th node in the linked list, if the index is valid.
-        """
         traverse_old = self.head
+        traverse_num = self.head
         new_list = MyLinkedList()
         traverse_new = new_list.head
-        
+
+        j = 0
+        while traverse_num:
+            traverse_num = traverse_num.next_node
+            j += 1
+
+        j -= 1
+
         i = 0
         while traverse_old:
             if index == i:
@@ -105,20 +111,12 @@ class MyLinkedList:
             else:
                 traverse_new.val = traverse_old.val
                 traverse_old = traverse_old.next_node
-                if traverse_old.next_node:
-                    traverse_new.next_node = Node(None)
-                    traverse_new = traverse_new.next_node
-                else:
-                    break
+                if traverse_old:
+                    if index == j and i == j - 1:
+                        break
+                    else:
+                        traverse_new.next_node = Node(None)
+                        traverse_new = traverse_new.next_node
+
             i += 1
-
         self.head = new_list.head
-
-
-    
-    # Helper function to traverse the list
-    def traverseList(self):
-        traverse = self.head
-        while traverse:
-            print(traverse.val)
-            traverse = traverse.next_node   
