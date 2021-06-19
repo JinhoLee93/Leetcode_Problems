da37750a2ed7b33fe83beb7cd07fd0845ecfe94d
Jinho Lee <jl5027@columbia.edu>
2021-06-19 23:34:23 +0900

    Update remove_duplicates_sorted_list.py

--- easy/remove_duplicates_sorted_list.py
+++ easy/remove_duplicates_sorted_list.py
@@ -5,4 +5,31 @@
 #         self.next = next
 class Solution:
     def deleteDuplicates(self, head: ListNode) -> ListNode:
+        cur = head
+        new = ListNode(None)
+        re = new
         
+        while cur:
+            if cur.next:
+                if cur.val == cur.next.val:
+                    new.val = cur.val
+                    while cur.val == cur.next.val:
+                        cur = cur.next
+                    
+                    if cur.next:
+                        cur = cur.next
+
+                else:
+                    new.val = cur.val
+                    cur = cur.next
+            
+            else:
+                new.val = cur.val
+                cur = cur.next
+            
+            if cur:
+                new.next = ListNode(None)
+                new = new.next
+     
+            
+        return re
