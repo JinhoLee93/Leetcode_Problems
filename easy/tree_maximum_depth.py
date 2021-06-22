2a31ef572b1760c4984c23745e29ddb8119f2a24
Jinho Lee <jl5027@columbia.edu>
2021-06-22 20:46:02 +0900

    Create tree_maximum_depth.py

---
+++ easy/tree_maximum_depth.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def check(self, r1, r2):
+        if r1 == None and r2 == None: 
+            return True
+        
+        if r1 == None or r2 == None: 
+            return False 
+        
+        if r1.val == r2.val:
+            return self.check(r1.left, r2.right) and\
+                   self.check(r1.right, r2.left)
+        
+    def isSymmetric(self, root: TreeNode) -> bool:
+        
+        return self.check(root.left, root.right)
+        
