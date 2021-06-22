c67aa2e33e9851d47eab65d423da547c89f02de2
Jinho Lee <jl5027@columbia.edu>
2021-06-22 23:29:52 +0900

    Update tree_maximum_depth.py

--- easy/tree_maximum_depth.py
+++ easy/tree_maximum_depth.py
@@ -1,15 +1,16 @@
+# Runtime: 36 ms, Memory usage: 16 MB
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution:
-    def depth(self, root, i): 
-        i += 1
-        
-        return self.depth(root.left, root.right, i)
+class Solution: 
     def maxDepth(self, root: TreeNode) -> int:
-        i = 0
+        if root == None: 
+            return 0
         
+        ld = self.maxDepth(root.left) + 1
+        rd = self.maxDepth(root.right) + 1
         
+        return max(ld, rd)
