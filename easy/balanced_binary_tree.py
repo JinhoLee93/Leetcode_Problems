952d5c8bab476be2b48b2b3e1dd54697bdda0502
Jinho Lee <jl5027@columbia.edu>
2021-06-26 22:26:47 +0900

    Update balanced_binary_tree.py

--- easy/balanced_binary_tree.py
+++ easy/balanced_binary_tree.py
@@ -5,5 +5,23 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def isBalanced(self, root: TreeNode) -> bool:
+    def check(self, root, depth):
+        depth += 1
         
+        if root.left == None and root.right == None:
+            
+        
+    def isBalanced(self, root: TreeNode) -> bool:
+        if root == None:
+            return True 
+        else: 
+            depth_left = 1
+            depth_right = 1
+            
+            ml = self.check(root.left, depth_left)
+            mr = self.check(root.right, depth_right)
+            
+            if abs(ml-mr) < 1:
+                return True
+            else:
+                return False
