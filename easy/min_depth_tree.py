e2bb70907546a203246ba6c990eadb0ece621255
Jinho Lee <jl5027@columbia.edu>
2021-06-30 21:31:30 +0900

    Update min_depth_tree.py

--- easy/min_depth_tree.py
+++ easy/min_depth_tree.py
@@ -6,4 +6,16 @@
 #         self.right = right
 class Solution:
     def minDepth(self, root: TreeNode) -> int:
+        if root == None:
+            return 0
         
+        left = self.minDepth(root.left) + 1
+        right = self.minDepth(root.right) + 1
+        
+        if left == 1 and right > 1:
+            return right
+        
+        if right == 1 and left > 1:
+            return left
+        
+        return min(left, right)
