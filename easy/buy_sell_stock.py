863826c0ee172af222a3617bb67cf8a352250ce4
Jinho Lee <jl5027@columbia.edu>
2021-07-11 19:09:52 +0900

    Update buy_sell_stock.py

--- easy/buy_sell_stock.py
+++ easy/buy_sell_stock.py
@@ -1,3 +1,18 @@
+# Faster than 95% and less memory usage than 55%
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
+        buy = float('inf')
+        sell = buy
+        net = 0
+
+        for i in range(len(prices)): 
+            if buy > prices[i]:
+                buy = prices[i]
+                sell = buy
+            
+            if sell < prices[i]:
+                sell = prices[i]
+                if (sell - buy) > net:
+                    net = sell - buy
         
+        return net
