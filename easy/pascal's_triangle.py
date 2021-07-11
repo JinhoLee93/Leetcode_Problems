17d301e79a3e20708c1f1884d47b917ac03f820e
Jinho Lee <jl5027@columbia.edu>
2021-07-11 16:25:13 +0900

    Update pascal's_triangle.py

--- easy/pascal's_triangle.py
+++ easy/pascal's_triangle.py
@@ -1,3 +1,20 @@
+# Runtime: 28 ms, Memory usage: 14.3 MB
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
+        tri = list()
         
+        for i in range(numRows): 
+            if i == 0:
+                tri.append([1])
+                
+            else:
+                sub = list()
+                sub.append(1)
+                
+                for j in range(len(tri[i - 1]) - 1):
+                    sub.append(tri[i - 1][j] + tri[i - 1][j + 1])
+                        
+                sub.append(1)
+                tri.append(sub)
+        
+        return tri
