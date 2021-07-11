06c4025a14887d420b770ef8c8739c295439e698
Jinho Lee <jl5027@columbia.edu>
2021-07-11 16:51:52 +0900

    Update pascal's_triangle_ii.py

--- easy/pascal's_triangle_ii.py
+++ easy/pascal's_triangle_ii.py
@@ -1,3 +1,19 @@
+# Runtime: 28 ms, Memory usage: 14.2 MB
 class Solution:
     def getRow(self, rowIndex: int) -> List[int]:
+        tri = list()
         
+        tri.append([1])
+        
+        for i in range(rowIndex + 1):
+            if i != 0:
+                sub = list()
+                sub.append(1)
+                for j in range(len(tri[i - 1]) - 1):
+                    sub.append(tri[i - 1][j] + tri[i - 1][j + 1])
+                
+                sub.append(1) 
+                
+                tri.append(sub)
+                
+        return tri[rowIndex]
