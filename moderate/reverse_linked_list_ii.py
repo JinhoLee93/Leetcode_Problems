a15e61bb7b1e096b8c5e5041a24aa8a5c959bf24
Jinho Lee <jl5027@columbia.edu>
2021-07-12 21:47:51 +0900

    Update reverse_linked_list_ii.py

--- moderate/reverse_linked_list_ii.py
+++ moderate/reverse_linked_list_ii.py
@@ -3,77 +3,46 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-def add_at_head(head, val):
-    traverse = head
-    new_head = ListNode(val)
-    pointer = new_head
-
-    while traverse:
-        new_head.next = traverse
-        traverse = traverse.next
-        new_head = new_head.next
-
-    return pointer
-    
-    
-def add_at_tail(head, val):
-    traverse = head
-    new_tail = ListNode(None)
-    pointer = new_tail
-
-    while traverse: 
-        new_tail.val = traverse.val
-        new_tail.next = ListNode(None)
-        new_tail = new_tail.next
-        traverse = traverse.next
-        
-    new_tail.val = val
-        
-    return pointer
-
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+def traverse(head): 
+    while head:
+        print(head.val) 
+        head = head.next
 
 class Solution:
     def reverseBetween(self, head: ListNode, left: int, right: int) -> ListNode:
-        cur = head
-        cur2 = head
-        front_nodes = list()
-        back_nodes = list()
+        if not head: 
+            return None 
         
-        while cur2:
-            if cur2.val == left:
-                while cur2.val != right:
-                    cur2 = cur2.next
+        cur, prv = head, None
+    
+        while cur.val != left: 
+            prv = cur
+            cur = cur.next
             
-            front_nodes.append(cur2.val)
-            cur2 = cur2.next
+        tail, con = cur, prv
         
-        while cur: 
-            if cur.val == left:
-                prv = None
-                while cur.val != right: 
-                    nxt = cur.next
-                    cur.next = prv
-                    prv = cur
-                    cur = nxt
-                nxt = cur.next
-                cur.next = prv
-                prv = cur
-                cur = nxt
-                cur = prv
-                break
-            else:
-                cur = cur.next
+        while cur.val != right: 
+            nxt = cur.next
+            cur.next = prv
+            prv = cur
+            cur = nxt
         
-        front_nodes = front_nodes[::-1]
-        back_nodes = back_nodes[::-1]
-        print(front_nodes)
-        print(back_nodes)
-
-        for i in range(len(front_nodes)):
-            cur = add_at_head(cur, front_nodes[i])
+        nxt = cur.next 
+        cur.next = prv
+        prv = cur
+        cur = nxt
         
-        for i in range(len(back_nodes)):
-            cur = add_at_tail(cur, back_nodes[i])
-            
+        if con:
+            con.next = prv
         
-        return cur
+        else:
+            head = prv
+        
+        tail.next = cur
+        
+        return head 
