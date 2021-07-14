ecd129f9aa8e598bd41373d5cf9e9baed27b4cb4
Jinho Lee <jl5027@columbia.edu>
2021-07-14 19:48:14 +0900

    Update longest_substring.py

--- moderate/longest_substring.py
+++ moderate/longest_substring.py
@@ -1,29 +1,14 @@
-class Node:
-    def __init__(self, val=None, child=None, count=0): 
-        val = val
-        child = child 
-        count = count
-
-class trie:
-    def __init__(self):
-        head = None
-    
-    def build(self, string): 
-        self.head = Node()
-        
-        for letter in string: 
-            
-    
-
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        if s == "":
-            return 0 
-        
-        
-        tri = trie()
-        tri.build(s)
-        
-        
-        
-        
+        chars = {}
+        count = 0
+
+        for char in s:
+            if char not in chars:
+                chars[char] = 0
+                count += 1
+            
+            else:
+                count -= 1
+            
+        return count
