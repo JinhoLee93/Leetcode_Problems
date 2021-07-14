6098e4a6893579ddcb480e4ef2d8a1534e25c904
Jinho Lee <jl5027@columbia.edu>
2021-07-14 20:19:42 +0900

    Update longest_substring.py

--- moderate/longest_substring.py
+++ moderate/longest_substring.py
@@ -2,13 +2,15 @@ class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
         chars = {}
         count = 0
+        max = 0
 
         for char in s:
             if char not in chars:
                 chars[char] = 0
                 count += 1
+                if count > max:
+                    max = count
+            else: 
+                count = 1
             
-            else:
-                count -= 1
-            
-        return count
+        return max
