01807dcd694ab2cb256982d87f71fa7fb13c4b05
Jinho Lee <jl5027@columbia.edu>
2021-07-15 18:44:06 +0900

    Update longest_substring.py

--- moderate/longest_substring.py
+++ moderate/longest_substring.py
@@ -1,19 +1,15 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
         chars = deque()
-        win1 = 0 
-        win2 = 0
+        win = 0
         maximum = 0
         
-        for char in s: 
-            if char not in chars:
-                chars.append(char)
-                maximum = max(maximum, win1 - win2 + 1)
-                win1 += 1
+        for i in range(len(s)): 
+            while s[i] in chars:
+                chars.popleft()
+                win += 1
             
-            else:
-                while char in chars:
-                    chars.popleft()
-                    win2 += 1
-        
+            chars.append(s[i])
+            maximum = max(maximum, i - win + 1)
+            
         return maximum
