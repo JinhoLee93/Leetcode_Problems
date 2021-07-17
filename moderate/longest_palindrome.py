1f73da9b3e64b55de1b959e08551f758cee1482d
Jinho Lee <jl5027@columbia.edu>
2021-07-17 22:43:13 +0900

    Update longest_palindrome.py

--- moderate/longest_palindrome.py
+++ moderate/longest_palindrome.py
@@ -1,7 +1,23 @@
+# Solution conceived by YouTuber NeetCode (https://www.youtube.com/watch?v=XYQecbcd6_c&t=173s&ab_channel=NeetCode)
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        palindrome = s[::-1]
-        win1 = 0
-        maximum = 0
+        ans = "" 
+        anslen = 0 
         
-        for i in range(len(palindrome)):
+        for i in range(len(s)): 
+            r, l = i, i
+            while 0 <= l and r < len(s) and s[l] == s[r]: 
+                if r - l + 1 > anslen:
+                    ans = s[l:r+1]
+                    anslen = r - l + 1
+                l -= 1
+                r += 1
+            
+            while 0 <= l and r < len(s) and s[l] == s[r]: 
+                if r - l + 1 > anslen:
+                    ans = s[l:r+1]
+                    anslen = r - l + 1
+                l -= 1
+                r += 1
+                
+        return ans
