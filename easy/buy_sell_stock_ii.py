ab51cd430decb12288136cae80e1cd3fa9ddb275
Jinho Lee <jl5027@columbia.edu>
2021-07-19 19:51:26 +0900

    Update buy_sell_stock_ii.py

--- easy/buy_sell_stock_ii.py
+++ easy/buy_sell_stock_ii.py
@@ -1,3 +1,13 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
+        win1 = 0
+        profit = 0
         
+        for i in range(len(prices)): 
+            if win1 < i:
+                if prices[i] > prices[win1]:
+                    profit += prices[i] - prices[win1]
+                
+                win1 += 1
+            
+        return profit
