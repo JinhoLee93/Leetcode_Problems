5a2d1cd30a75c5170589d653e0b6e63f31c8d75b
Jinho Lee <jl5027@columbia.edu>
2021-07-21 00:37:47 +0900

    Create excel_column.py

---
+++ easy/excel_column.py
@@ -0,0 +1,10 @@
+class Solution:
+    def convertToTitle(self, n: int) -> str:
+        res = ""
+        chars = list(string.ascii_uppercase)
+        
+        while n > 0:
+            res += chars[(n - 1) % 26]
+            n = (n - 1) // 26
+            
+        return res[::-1]
