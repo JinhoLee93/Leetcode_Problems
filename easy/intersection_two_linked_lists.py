8aa0d4877438ca0308f7f28e27a47bd7f1ba5fd2
Jinho Lee <jl5027@columbia.edu>
2021-07-20 20:46:00 +0900

    Update intersection_two_linked_lists.py

--- easy/intersection_two_linked_lists.py
+++ easy/intersection_two_linked_lists.py
@@ -6,4 +6,22 @@
 
 class Solution:
     def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> ListNode:
+        if not headA or not headB:
+            return None
         
+        p1 = headA
+        p2 = headB
+        
+        # two pointers will catch up after the first loop
+        while p1 != p2: 
+            if not p1:
+                p1 = headB
+            else:
+                p1 = p1.next
+            
+            if not p2:
+                p2 = headA
+            else:
+                p2 = p2.next
+                
+        return p1 
