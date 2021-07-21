ed9ea0a69e95b372ed08ad37dc504bf3af763944
Jinho Lee <jl5027@columbia.edu>
2021-07-21 21:17:04 +0900

    Update factorial_trailing_zeroes.py

--- easy/factorial_trailing_zeroes.py
+++ easy/factorial_trailing_zeroes.py
@@ -1,3 +1,9 @@
 class Solution:
     def trailingZeroes(self, n: int) -> int:
+        denom = 5
+        res = 0
+        while denom <= n:
+            res += n//denom
+            denom *= 5
         
+        return res
