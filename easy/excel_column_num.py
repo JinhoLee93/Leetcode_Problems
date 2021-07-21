a16cd83f0c6d6d6c8ff5d1c1107f85da519ec0bc
Jinho Lee <jl5027@columbia.edu>
2021-07-21 18:50:56 +0900

    Update excel_column_num.py

--- easy/excel_column_num.py
+++ easy/excel_column_num.py
@@ -1,2 +1,11 @@
 class Solution:
     def titleToNumber(self, columnTitle: str) -> int:
+        chars = list(string.ascii_uppercase)
+        res = 0
+        
+        for i in range(len(columnTitle)):
+            
+            res += 26 ** ((len(columnTitle) - 1) - i) * (chars.index(columnTitle[i]) + 1)
+            
+            
+        return res
