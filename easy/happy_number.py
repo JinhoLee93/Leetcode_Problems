45f44a79837becf8e3c6a7fa28c43de438aed383
Jinho Lee <jl5027@columbia.edu>
2021-07-21 23:48:51 +0900

    Update happy_number.py

--- easy/happy_number.py
+++ easy/happy_number.py
@@ -1,3 +1,20 @@
+# Faster than 100%
 class Solution:
     def isHappy(self, n: int) -> bool:
+        cycle = set()
         
+        while n != 1:
+            res = 0
+            n = str(n)
+            
+            for i in range(len(n)):
+                res += int(n[i]) ** 2
+
+            if res not in cycle:
+                cycle.add(res)
+                n = res
+            
+            else:
+                return False
+            
+        return True
