628aff6c11f2f3e8b6116f9da67656117ccbbf20
Jinho Lee <jl5027@columbia.edu>
2021-07-22 23:42:08 +0900

    Update contain_duplicate_ii.py

--- easy/contain_duplicate_ii.py
+++ easy/contain_duplicate_ii.py
@@ -1,3 +1,17 @@
 class Solution:
     def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        hashmap = {}
         
+        for i in range(len(nums)): 
+            if nums[i] not in hashmap:
+                hashmap[nums[i]] = i
+            
+            else:
+                hashmap[nums[i]] = abs(hashmap[nums[i]] - i)
+                if hashmap[nums[i]] <= k:
+                    return True
+                
+                else:
+                    hashmap[nums[i]] = i
+        
+        return False
