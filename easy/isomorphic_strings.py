ea7b1990869bff767d4675af5f8c4d2f4f9a0b8b
Jinho Lee <jl5027@columbia.edu>
2021-07-22 23:01:15 +0900

    Update isomorphic_strings.py

--- easy/isomorphic_strings.py
+++ easy/isomorphic_strings.py
@@ -1,3 +1,15 @@
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
+        hashmap = {}
         
+        for i in range(len(s)):
+            if s[i] not in hashmap.keys() and t[i] not in hashmap.values():
+                hashmap[s[i]] = t[i]
+            
+            elif s[i] not in hashmap.keys() and t[i] in hashmap.values():
+                return False
+            
+            if  hashmap[s[i]] != t[i]:
+                return False
+            
+        return True
