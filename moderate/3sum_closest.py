3ef70a5d2032cc89b5aec43a4ed8dee2511101c3
Jinho Lee <jl5027@columbia.edu>
2021-07-28 19:19:34 +0900

    Update 3sum_closest.py

--- moderate/3sum_closest.py
+++ moderate/3sum_closest.py
@@ -1,21 +1,24 @@
 class Solution:
     def threeSumClosest(self, nums: List[int], target: int) -> int:
         nums = sorted(nums)
-        res = []
-        minimum = None
+        res = float('inf')
+        mindis = float('inf')
         
         for i in range(len(nums)):
             j, k = i + 1, len(nums) - 1
             
             while j < k:
-                minimum = target - (nums[i] + nums[j] + nums[k])
+                three = nums[i] + nums[j] + nums[k]
+                dis = abs(target - three) 
+                if mindis > dis: 
+                    mindis = dis
+                    res = three
                 
-                if minimum > target:
+                if three > target:
                     k -= 1
-                
-                elif minumum < target:
+                elif three < target:
                     j += 1
+                else:
+                    return nums[i] + nums[j] + nums[k]
                 
-                else: 
-                    
-            
+        return res
