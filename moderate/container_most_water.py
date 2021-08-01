32ed24a7b84a49c682162ed8084131631dd9dee4
Jinho Lee <jl5027@columbia.edu>
2021-08-01 19:33:44 +0900

    Update container_most_water.py

--- moderate/container_most_water.py
+++ moderate/container_most_water.py
@@ -3,18 +3,16 @@ class Solution:
         if len(height) == 2:
             return min(height)
         
-        most_water = 0
         l, r = 0, len(height) - 1
+        most_water = 0
+        
         while l < r:
+            temp = (r - l) * min(height[l], height[r])
             if height[l] < height[r]:
-                temp = (r - l) * min(height[l], height[r])
                 l += 1
-            
             else:
-                temp = (r - l) * min(height[l], height[r])
                 r -= 1
-                
+            
             most_water = max(most_water, temp)
             
         return most_water
-            
