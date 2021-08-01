9d57de385d844630f9b40dd3e1464844c477f16d
Jinho Lee <jl5027@columbia.edu>
2021-08-01 17:39:08 +0900

    Update 3sum.py

--- moderate/3sum.py
+++ moderate/3sum.py
@@ -1,29 +1,32 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        if len(nums) < 3:
-            return None
+        nums = sorted(nums)
+        three = 0
+        res = []
         
-        else:
-            nums = sorted(nums)
-            l1 = []
+        for i in range(len(nums)):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
             
-            for i in range(len(nums)):
-                j, k = i + 1, len(nums) - 1
+            j, k = i + 1, len(nums) - 1
+            
+            while j < k:
+                three = nums[i] + nums[j] + nums[k]
                 
-                while j < k:
-                    three_sum = nums[i] + nums[j] + nums[k]
+                if three > 0:
+                    k -= 1
+                
+                elif three < 0:
+                    j += 1
+                
+                else:
+                    res.append([nums[i], nums[j], nums[k]])
+                    j += 1
+                    k -= 1
+                    while j < k and nums[j] == nums[j - 1]:
+                        j += 1
                     
-                    if three_sum > 0:
+                    while j < k and nums[k] == nums[k + 1]:
                         k -= 1
-                    
-                    elif three_sum < 0:
-                        j += 1
-                        
-                    else:
-                        if [nums[i], nums[j], nums[k]] not in l1:
-                            l1.append([nums[i], nums[j], nums[k]])
-                        j += 1
-                        while j < k and nums[j] == nums[j - 1]:
-                            j += 1
-            
-            return l1
+        
+        return res
