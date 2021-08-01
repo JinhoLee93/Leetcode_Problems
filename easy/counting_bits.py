2a73a076953626b96a2f1f2820929093c445ac39
Jinho Lee <jl5027@columbia.edu>
2021-08-02 00:19:00 +0900

    Update counting_bits.py

--- easy/counting_bits.py
+++ easy/counting_bits.py
@@ -6,3 +6,18 @@ class Solution:
             res[i] = str(bin(i)[2:]).count("1")
             
         return res
+    
+# DP
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        res = [0] * (n + 1)
+        offset = 1
+        
+        for i in range(1, n + 1):
+            if offset * 2 == i:
+                offset = i
+                
+            res[i] = 1 + res[i - offset]
+            
+        return res
+            
