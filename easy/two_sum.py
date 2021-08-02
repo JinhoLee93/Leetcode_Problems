9a69f8c76d92916cdd387409722e7b3b6e8508a9
Jinho Lee <jl5027@columbia.edu>
2021-08-02 16:38:02 +0900

    Update two_sum.py

--- easy/two_sum.py
+++ easy/two_sum.py
@@ -1,10 +1,11 @@
-# Runtime: 800 ms, Memory: 14.8 MB
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        # Big O should be < O(n^2)
-        # Can be done by a simple list apprehension.
+        hashmap = {}
+        
         for i in range(len(nums)):
-            t = target - nums[i]
+            if nums[i] not in hashmap: 
+                hashmap[nums[i]] = i
             
-            if t in nums and i != nums.index(t):
-                return [i, nums.index(t)]
+            if (target - nums[i]) in hashmap:
+                if hashmap[target - nums[i]] != i:
+                    return [hashmap[target - nums[i]], i]
