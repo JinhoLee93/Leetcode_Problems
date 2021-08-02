77c36069f8bd6e8476a225315a1989c7b6bfa708
Jinho Lee <jl5027@columbia.edu>
2021-08-03 00:48:56 +0900

    Update binary_tree_level_order.py

--- moderate/binary_tree_level_order.py
+++ moderate/binary_tree_level_order.py
@@ -6,4 +6,34 @@
 #         self.right = right
 class Solution:
     def levelOrder(self, root: TreeNode) -> List[List[int]]:
+        if root == None:
+            return [] 
         
+        q = deque() 
+        q.append(root)
+        level = 0
+        
+        while q:
+            level += 1
+            for i in range(len(q)):
+                cur = q.popleft()
+                if cur.left:
+                    q.append(cur.left)
+                
+                if cur.right:
+                    q.append(cur.right)
+                    
+        res = [[] for i in range(level)]
+        
+        def dfs(root, res, level):
+            if root == None:
+                return None
+            
+            res[level].append(root.val)
+            
+            dfs(root.left, res, level + 1)
+            dfs(root.right, res, level + 1)
+            
+        dfs(root, res, 0)
+        
+        return res
