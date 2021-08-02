bc006bcf146ff220eb31f4fee3eb0996854069b3
Jinho Lee <jl5027@columbia.edu>
2021-08-02 19:15:28 +0900

    Update clone_graph.py

--- moderate/clone_graph.py
+++ moderate/clone_graph.py
@@ -5,7 +5,27 @@ class Node:
         self.val = val
         self.neighbors = neighbors if neighbors is not None else []
 """
-
+# BFS
 class Solution:
     def cloneGraph(self, node: 'Node') -> 'Node':
+        if node == None:
+            return node 
         
+        clones = {} 
+        clones[node] = Node(node.val)
+        q = collections.deque()
+        q.append(node)
+        
+        while q: 
+            cur = q.popleft()
+            for neighbor in cur.neighbors:
+                if neighbor not in clones:
+                    clones[neighbor] = Node(neighbor.val)
+                    q.append(neighbor)
+                
+                clones[cur].neighbors.append(clones[neighbor])
+            
+
+        return clones[node]
+
+# DFS
