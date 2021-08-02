2d8b5d7a73775c55524d9024889cc995e72beb1d
Jinho Lee <jl5027@columbia.edu>
2021-08-02 22:22:57 +0900

    Update invert_binary_tree.py

--- easy/invert_binary_tree.py
+++ easy/invert_binary_tree.py
@@ -5,6 +5,7 @@
 #         self.left = left
 #         self.right = right
 class Solution:
+    # DFS
     def invertTree(self, root: TreeNode) -> TreeNode:
         if root == None:
             return None
@@ -16,4 +17,25 @@ class Solution:
         self.invertTree(root.left)
         self.invertTree(root.right)
         
+        return root
+    
+    # BFS
+    def invertTree(self, root: TreeNode) -> TreeNode:
+        if root == None:
+            return None
+        
+        q = deque()
+        q.append(root)
+        while q:
+            cur = q.popleft()
+            temp = cur.left
+            cur.left = cur.right
+            cur.right = temp
+            
+            if cur.left:
+                q.append(cur.left)
+            
+            if cur.right:
+                q.append(cur.right)
+                
         return root
