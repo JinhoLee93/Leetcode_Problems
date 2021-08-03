aa698618d2d9f1d4386b9cbe736502340fd8fc76
Jinho Lee <jl5027@columbia.edu>
2021-08-03 15:56:51 +0900

    Update construct_pre_in_order.py

--- moderate/construct_pre_in_order.py
+++ moderate/construct_pre_in_order.py
@@ -7,3 +7,17 @@
 class Solution:
     def buildTree(self, preorder: List[int], inorder: List[int]) -> TreeNode:
         
+        #preorder: [root.val] + self.buildTree(root.left) + self.buildTree(root.right)
+        
+        #inorder: self.buildTree(root.left) + [root.val] + self.buildTree(root.right)
+        
+        if not inorder or not preorder:
+            return None
+        
+        root = TreeNode(preorder[0])
+        in_root_idx = inorder.index(preorder[0])
+        
+        root.left = self.buildTree(preorder[1:in_root_idx + 1], inorder[0:in_root_idx])
+        root.right = self.buildTree(preorder[in_root_idx + 1: len(preorder)], inorder[in_root_idx + 1: len(inorder)])
+        
+        return root
