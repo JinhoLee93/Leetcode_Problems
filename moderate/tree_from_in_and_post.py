17daa243d1ded99b69f47a7b3a4dda0467fa8bb7
Jinho Lee <jl5027@columbia.edu>
2021-08-03 16:51:33 +0900

    Update tree_from_in_and_post.py

--- moderate/tree_from_in_and_post.py
+++ moderate/tree_from_in_and_post.py
@@ -6,4 +6,15 @@
 #         self.right = right
 class Solution:
     def buildTree(self, inorder: List[int], postorder: List[int]) -> TreeNode:
+        if not inorder or not postorder:
+            
+            return None 
         
+        root = TreeNode(postorder[-1])
+        
+        in_root_idx = inorder.index(postorder[-1])
+        
+        root.left = self.buildTree(inorder[0:in_root_idx], postorder[0:in_root_idx])
+        root.right = self.buildTree(inorder[in_root_idx + 1:len(inorder)], postorder[in_root_idx:len(postorder) - 1])
+         
+        return root
