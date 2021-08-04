8184ec37971ee580142b312636ecd26e27202886
Jinho Lee <jl5027@columbia.edu>
2021-08-05 01:21:08 +0900

    Update implement_trie.py

--- moderate/implement_trie.py
+++ moderate/implement_trie.py
@@ -1,3 +1,9 @@
+class TrieNode:
+
+    def __init__(self):
+        self.children = {}
+        self.is_last_chr = False
+
 class Trie:
 
     def __init__(self):
@@ -5,23 +11,56 @@ class Trie:
         Initialize your data structure here.
         """
         
+        self.trie = TrieNode()
+        
 
     def insert(self, word: str) -> None:
         """
         Inserts a word into the trie.
         """
+        cur = self.trie
+        
+        for c in word:
+            if c not in cur.children:
+                cur.children[c] = TrieNode()
+            
+            cur = cur.children[c]
+            
+        cur.is_last_chr = True
+        
         
 
     def search(self, word: str) -> bool:
         """
         Returns if the word is in the trie.
         """
+        cur = self.trie
+        
+        for c in word:
+            if c not in cur.children:
+                
+                return False
+            
+            cur = cur.children[c]
+            
+        
+        return cur.is_last_chr
         
 
     def startsWith(self, prefix: str) -> bool:
         """
         Returns if there is any word in the trie that starts with the given prefix.
         """
+        cur = self.trie
+        
+        for c in prefix:
+            if c not in cur.children:
+                
+                return False
+            
+            cur = cur.children[c]
+            
+        return True
         
 
 
