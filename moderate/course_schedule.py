11a68c18417d09150e9cc01f71b1bef2b1ddc9c3
Jinho Lee <jl5027@columbia.edu>
2021-08-07 02:01:22 +0900

    Update course_schedule.py

--- moderate/course_schedule.py
+++ moderate/course_schedule.py
@@ -1,3 +1,36 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        prereqmap = {i: [] for i in range(numCourses)}
         
+        for course, prereq in prerequisites:
+            prereqmap[course].append(prereq)
+        
+        visitset = set()
+        
+        def dfs(course):
+            if course in visitset:
+                
+                return False
+            
+            if prereqmap[course] == None:
+                
+                return True
+            
+            visitset.add(course)
+            
+            for prereq in prereqmap[course]:
+                if not dfs(prereq):
+                    
+                    return False
+            
+            visitset.remove(course)
+            prereqmap[course] = None
+            
+            return True 
+        
+        for course in range(numCourses):
+            if not dfs(course):
+                
+                return False
+            
+        return True
