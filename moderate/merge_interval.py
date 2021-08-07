309f70f8cd7061218c06ae596139649e042dec79
Jinho Lee <jl5027@columbia.edu>
2021-08-08 01:55:44 +0900

    Update merge_interval.py

--- moderate/merge_interval.py
+++ moderate/merge_interval.py
@@ -1,3 +1,16 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        # Time: O(N), Space: O(N)
+        intervals.sort(key = lambda i:i[0])
+        merged = [intervals[0]]
         
+        for start, end in intervals[1:]:
+            mergepoint = merged[-1][1]
+            
+            if mergepoint >= start:
+                merged[-1][1] = max(mergepoint, end)
+            
+            else:
+                merged.append([start, end])
+            
+        return merged
