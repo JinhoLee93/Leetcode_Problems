179d9a271be90f502bb2d9afbe9ebe44db405442
Jinho Lee <jl5027@columbia.edu>
2021-08-07 17:13:03 +0900

    Update course_schedule_ii.py

--- moderate/course_schedule_ii.py
+++ moderate/course_schedule_ii.py
@@ -1,3 +1,45 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        # Time: O(M + N)
+        # Space: O(M + N)
         
+        prereqmap = {i: [] for i in range(numCourses)}
+        
+        for course, prereq in prerequisites:
+            prereqmap[course].append(prereq)
+            
+        courseset = set() 
+        
+        result = []
+        
+        def dfs(course):
+            if course in courseset:
+                
+                return False
+            
+            if prereqmap[course] == None:
+                
+                return True
+            
+            courseset.add(course) 
+            
+            for prereq in prereqmap[course]:
+                if not dfs(prereq):
+                    
+                    return False
+                
+                if prereq not in result:
+                    result.append(prereq)
+            
+            courseset.remove(course)
+            prereqmap[course] = None
+            result.append(course)
+                
+            return True
+            
+        for course in range(numCourses):
+            if not dfs(course):
+
+                return []
+        
+        return result
