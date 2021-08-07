89b76b60174cccbd80e9d8babb6743ac401c7911
Jinho Lee <jl5027@columbia.edu>
2021-08-07 19:03:02 +0900

    Update longest_consecutive_sequence.py

--- moderate/longest_consecutive_sequence.py
+++ moderate/longest_consecutive_sequence.py
@@ -1,3 +1,18 @@
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
+        numSet = set(nums)
+        longest = 0
         
+        for n in numSet:
+            if n - 1 not in numSet:
+                cur = n - 1
+                long = 0
+                
+                while cur + 1 in numSet:
+                    cur += 1
+                    long += 1
+                    
+            
+                longest = max(longest, long)
+            
+        return longest
