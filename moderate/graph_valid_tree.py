3fdc53d25098f3507efce700f7dddfa2451079e6
Jinho Lee <jl5027@columbia.edu>
2021-08-16 18:21:17 +0900

    Create graph_valid_tree.py

---
+++ moderate/graph_valid_tree.py
@@ -0,0 +1,32 @@
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        # Time: O(E(number of edges) + V(number of vertices)), Space: O(E + V)
+        if not n:
+            
+            return True
+        
+        adj = {i:[] for i in range(n)}
+        visitSet = set()
+        
+        for root, child in edges:
+            adj[root].append(child)
+            adj[child].append(root)
+            
+            
+        def dfs(root, prv):
+            if root in visitSet:
+                
+                return False
+            
+            visitSet.add(root)
+            
+            for child in adj[root]:
+                if child == prv:
+                    continue
+                
+                if not dfs(child, root):
+                    return False
+                
+            return True
+        
+        return dfs(0, -1) and n == len(visitSet)
