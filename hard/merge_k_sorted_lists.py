24f7d8a760d8e4f270f89ebd22f99c71c460628c
Jinho Lee <jl5027@columbia.edu>
2021-08-17 21:04:42 +0900

    Update merge_k_sorted_lists.py

--- hard/merge_k_sorted_lists.py
+++ hard/merge_k_sorted_lists.py
@@ -5,4 +5,18 @@
 #         self.next = next
 class Solution:
     def mergeKLists(self, lists: List[ListNode]) -> ListNode:
+        # Brute Force: Time: O(NlogN), Space: O(N)
+        newList = ListNode(-1)
+        cur = newList
+        tempList = []
         
+        for l in lists:
+            while l:
+                tempList.append(l.val)
+                l = l.next
+                
+        for num in sorted(tempList): 
+            cur.next = ListNode(num)
+            cur = cur.next
+        
+        return newList.next
