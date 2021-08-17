cb417a5ec019d48b81198c88ca7aaec1321f90c8
Jinho Lee <jl5027@columbia.edu>
2021-08-17 19:59:23 +0900

    Update merge_two_sorted_lists.py

--- easy/merge_two_sorted_lists.py
+++ easy/merge_two_sorted_lists.py
@@ -1,24 +1,28 @@
-# Runtime: 36 ms, Memory usage: 14.3 MB
-# Merge lists with a simple recursion.
-
-#Definition for singly-linked list.
-#class ListNode:
-#    def __init__(self, val=0, next=None):
-#        self.val = val
-#        self.next = next
-        
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
     def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
-        if not l1:
-            return l2
+        newList = ListNode(-1) # Dummy value
+        cur = newList
         
-        if not l2:
-            return l1
-        
-        while l1 and l2: 
-            if l1.val <= l2.val: 
-                l1.next = self.mergeTwoLists(l1.next, l2)
-                return l1
+        while l1 and l2:
+            if l1.val >= l2.val:
+                cur.next = l2
+                l2 = l2.next
+                
             else:
-                l2.next = self.mergeTwoLists(l2.next, l1)
-                return l2
+                cur.next = l1
+                l1 = l1.next
+            
+            cur = cur.next
+        
+        if l1:
+            cur.next = l1
+        
+        elif l2:
+            cur.next = l2
+    
+        return newList.next
