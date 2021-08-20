eb79866160f5457a380768280a67e264530d2fa3
Jinho Lee <jl5027@columbia.edu>
2021-08-20 21:31:41 +0900

    Update top_k_frequent_elements.py

--- moderate/top_k_frequent_elements.py
+++ moderate/top_k_frequent_elements.py
@@ -1,6 +1,6 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        # Time = O(N), Space = O(N)
+        # Time = O(N), worst = O(N^2), Space = O(N)
         hashMap = {} 
         bucket = [[] for i in range(len(nums) + 1)]
         result = []
@@ -17,3 +17,9 @@ class Solution:
                 if len(result) == k:
                     
                     return result
+        
+        # Heap
+        # Time = O(NlogN), Space = O(N)
+        count = Counter(nums)
+        
+        return heapq.nlargest(k, count.keys(), key=count.get)
