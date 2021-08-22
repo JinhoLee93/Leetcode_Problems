33ca78977bba8ea0adf24d7158f2c3367863693d
Jinho Lee <jl5027@columbia.edu>
2021-08-22 17:31:11 +0900

    Update number_of_distinct_islands.py

--- moderate/number_of_distinct_islands.py
+++ moderate/number_of_distinct_islands.py
@@ -1,29 +1,33 @@
 class Solution:
     def numDistinctIslands(self, grid: List[List[int]]) -> int:
+        # Time: O(M+N) Space: O(M+N)
         ROWS, COLS = len(grid), len(grid[0])
-        islandSet = set()
-        numOfIsland = 0
+        seen = set()
+        uniqueIslands = set()
         
-        def dfs(r, c, i):
+        def dfs(r, c, direction):
             if r > ROWS - 1 or c > COLS - 1 or \
                 r < 0 or c < 0 or \
-                (r, c) in islandSet or \
+                (r, c) in seen or \
                 grid[r][c] != 1:
                 
                 return
             
-            islandSet.add((r, c))
-            dfs(r + 1, c, i)
-            dfs(r - 1, c, i)
-            dfs(r, c + 1, i)
-            dfs(r, c - 1, i)
+            seen.add((r, c))
+            curIsland.append(direction)
+            dfs(r + 1, c, 'D')
+            dfs(r - 1, c, 'U')
+            dfs(r, c + 1, 'R')
+            dfs(r, c - 1, 'L')
+            curIsland.append('O')
             
-        i = 0
         for r in range(ROWS):
             for c in range(COLS): 
                 if grid[r][c] == 1: 
-                    if (r, c) not in islandSet:
-                        dfs(r, c)
-                        i += 1
+                    if (r, c) not in seen:
+                        curIsland = []
+                        dfs(r, c, 'O')
+                        if curIsland:
+                            uniqueIslands.add(tuple(curIsland))
         
-        return numOfIsland 
+        return len(uniqueIslands)
