c1ac7893a77d8e901d11ff4dd6386c33ca34ff79
Jinho Lee <jl5027@columbia.edu>
2021-08-27 10:27:30 +0900

    Update range_sum_query.py

--- easy/range_sum_query.py
+++ easy/range_sum_query.py
@@ -1,15 +1,15 @@
 class NumArray:
 
     def __init__(self, nums: List[int]):
-        self.numArray = [0] * (len(nums) + 1)
+        self.dp = {0:0}
         
-        for i in range(1, len(self.numArray)):
-            self.numArray[i] = self.numArray[i - 1] + nums[i - 1]
-        
+        for idx, num in enumerate(nums): 
+            self.dp[idx + 1] = num + self.dp[idx]
+
     def sumRange(self, left: int, right: int) -> int:
-
-        return self.numArray[right + 1] - self.numArray[left]
-
+        
+        return self.dp[right + 1] - self.dp[left]
+        
 # Your NumArray object will be instantiated and called as such:
 # obj = NumArray(nums)
 # param_1 = obj.sumRange(left,right)
