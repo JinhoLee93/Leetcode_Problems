41718a8bf218d6fb954393e8a9fc58971e0880ce
Jinho Lee <jl5027@columbia.edu>
2021-08-29 10:52:11 +0900

    Update search_rotated_array.py

--- moderate/search_rotated_array.py
+++ moderate/search_rotated_array.py
@@ -1,20 +1,27 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        if len(nums) % 2 == 0:
-            l, r = (len(nums) // 2) - 1, len(nums) // 2
+        l, r = 0, len(nums) - 1
         
-        else:
-            l, r = len(nums) // 2, len(nums) // 2
+        while l <= r: 
+            mid = s + ((r - s) // 2) 
             
-        while l > -1 and r < len(nums):
-            if nums[l] == target:
-                return l
-            
-            if nums[r] == target:
-                return r
-            
-            l -= 1
-            r += 1
-            
+            if nums[mid] == target:
                 
+                return mid
+            
+            elif nums[l] <= nums[mid]:
+                if nums[l] <= target and nums[mid] < target:
+                    r = mid - 1
+                 
+                else:
+                    l = mid + 1
+                    
+            else:
+                if nums[r] > target and nums[mid] <= target:
+                    l = mid + 1
+                    
+                else:
+                    r = mid - 1
+
+                    
         return -1
