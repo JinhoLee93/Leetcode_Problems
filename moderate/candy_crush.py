27bee16625c096d9cbf850a59c2cc89a8b233323
Jinho Lee <jl5027@columbia.edu>
2021-08-29 23:47:27 +0900

    Update candy_crush.py

--- moderate/candy_crush.py
+++ moderate/candy_crush.py
@@ -1,61 +1,57 @@
 class Solution:
     def candyCrush(self, board: List[List[int]]) -> List[List[int]]:
-        # Time: O((R*C)^2), Space: O(R+C)
-        ROWS, COLS = len(board), len(board[0]) 
-        pathSet = set()
+        ROWS, COLS = len(board), len(board[0])
+        pathSet = set() 
         
-        def checkDown(r, c, val, i):
-            if r > (ROWS - 1) or \
-                board[r][c] != val:
+        def rowCheck(r, c, prv, rowArr):
+            if r > ROWS - 1 or \
+                board[r][c] != prv:
                 
-                return i
+                return 0
             
-            rows.append((r, c))
+            rowArr.append((r, c))
             
-            return checkDown(r + 1, c, val, i + 1)
+            return rowCheck(r + 1, c, board[r][c], rowArr) + 1
         
-        def checkRight(r, c, val, i):
-            if c > (COLS - 1) or \
-                board[r][c] != val:
+        def colCheck(r, c, prv, colArr):
+            if c > COLS - 1 or \
+                board[r][c] != prv:
                 
-                return i 
+                return 0
             
-            cols.append((r, c))
+            colArr.append((r, c))
             
-            return checkRight(r, c + 1, val, i + 1)
+            return colCheck(r, c + 1, board[r][c], colArr) + 1
             
-        
         while True:
             # Crush
             for r in range(ROWS):
                 for c in range(COLS):
-                    if board[r][c] == 0: 
-                        continue 
-                    
-                    rows = []
-                    cols = []
-                    rowDown = checkDown(r, c, board[r][c], 0)
-                    colRight = checkRight(r, c, board[r][c], 0)
-                    
-                    if rowDown > 2:
-                        for cell in rows:
-                            pathSet.add(cell)
+                    if board[r][c] != 0:
+                        rows = []
+                        cols = []
+
+                        if rowCheck(r, c, board[r][c], rows) > 2:
+                            for i in range(len(rows)):
+                                pathSet.add(rows[i])
+
+                        if colCheck(r, c, board[r][c], cols) > 2:
+                            for i in range(len(cols)):
+                                pathSet.add(cols[i])
                             
-                    if colRight > 2:
-                        for cell in cols:
-                            pathSet.add(cell)
-            
             if len(pathSet) == 0:
                 
-                break 
+                break
             
+            # Refill
             for r in range(ROWS):
                 for c in range(COLS):
-                    if (r, c) in pathSet: 
+                    if (r, c) in pathSet:
                         for i in range(r, 0, -1):
                             board[i][c] = board[i - 1][c]
-
-                        board[0][c] = 0
-                        pathSet.remove((r, c))
                             
+                        board[0][c] = 0
+            
+            pathSet.clear()
+        
         return board
