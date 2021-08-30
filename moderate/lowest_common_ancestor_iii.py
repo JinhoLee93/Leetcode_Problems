8e2e3791d41f2566645d0c2f4c49ca22dd7b2b2d
Jinho Lee <jl5027@columbia.edu>
2021-08-30 20:09:27 +0900

    Create lowest_common_ancestor_iii.py

---
+++ moderate/lowest_common_ancestor_iii.py
@@ -0,0 +1,31 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+        self.parent = None
+"""
+
+class Solution:
+    def lowestCommonAncestor(self, p: 'Node', q: 'Node') -> 'Node':
+        nodeSet = set()
+        
+        def dfs(node):
+            if not node:
+                
+                return
+            
+            if node in nodeSet:
+                
+                return node
+            
+            nodeSet.add(node)
+            
+            return dfs(node.parent)
+            
+            
+        dfs(p)
+        
+        return dfs(q)
