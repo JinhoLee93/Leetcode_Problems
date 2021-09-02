252b55d4d07bebbf0facb2d95aa940171d884558
Jinho Lee <jl5027@columbia.edu>
2021-09-03 00:04:39 +0900

    Update search_rotated_array.py

--- moderate/search_rotated_array.py
+++ moderate/search_rotated_array.py
@@ -4,7 +4,7 @@ class Solution:
         l, r = 0, len(nums) - 1
         
         while l <= r: 
-            mid = s + ((r - s) // 2) 
+            mid = l + ((r - l) // 2) 
             
             if nums[mid] == target:
                 
