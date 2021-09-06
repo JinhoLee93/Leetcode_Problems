326b8e87b318192b3824ec03ffec06242467c6d1
Jinho Lee <jl5027@columbia.edu>
2021-09-06 17:33:35 +0900

    Update reverse_string.py

--- easy/reverse_string.py
+++ easy/reverse_string.py
@@ -3,4 +3,15 @@ class Solution:
         """
         Do not return anything, modify s in-place instead.
         """
+        # API
         s = s.reverse()
+        
+        # Two Pointers
+        l, r = 0, len(s) - 1
+        while l <= r: 
+            tmp = s[r]
+            s[r] = s[l]
+            s[l] = tmp
+            
+            l += 1
+            r -= 1
