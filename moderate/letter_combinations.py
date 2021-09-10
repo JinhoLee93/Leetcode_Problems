22990a4ea31c88de40817054c711c566679e2584
Jinho Lee <jl5027@columbia.edu>
2021-09-10 17:48:30 +0900

    Update letter_combinations.py

--- moderate/letter_combinations.py
+++ moderate/letter_combinations.py
@@ -26,5 +26,46 @@ class Solution:
                 res.append(cur_str)
                 
         backtrack(0, "")
+          
+        return res
+    
+        #Brute Force
+        class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        if len(digits) == 0:
+            
+            return []
+        
+        keypad = {"2": ["a", "b", "c"],
+                  "3": ["d", "e", "f"],
+                  "4": ["g", "h", "i"],
+                  "5": ["j", "k", "l"],
+                  "6": ["m", "n", "o"],
+                  "7": ["p", "q", "r", "s"],
+                  "8": ["t", "u", "v"],
+                  "9": ["w", "x", "y", "z"]}
+        
+        def bruteforce(num, arr, i):
+            sub = []
+            
+            if i == 0:
+                for char in keypad[num]: 
+                    sub.append(char)
                 
+            else:
+                for char1 in arr:
+                    for char2 in keypad[num]:
+                        sub.append(char1 + char2)
+
+            return sub
+                
+        res = []
+
+        for i in range(len(digits)):
+            if digits[i] not in keypad:
+                
+                return []
+            
+            res = bruteforce(digits[i], res, i)
+            
         return res
