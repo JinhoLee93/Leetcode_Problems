883154c335f5d5c76dd4b014b5e7fa9273cfb9ac
Jinho Lee <jl5027@columbia.edu>
2021-09-10 18:22:10 +0900

    Update add_binary.py

--- easy/add_binary.py
+++ easy/add_binary.py
@@ -1,5 +1,42 @@
-# Runtime: 28ms, Memory usage: 14MB
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
-        sum = bin(int(a, 2) + int(b, 2)) 
-        return sum[2:]
+        n = max(len(a), len(b))
+        a, b = a.zfill(n), b.zfill(n)
+        carry = 0
+        res = ""
+        
+        for i in range(n - 1, -1, -1):
+            if a[i] == '0' and b[i] == '0':
+                if carry > 0:
+                    res += '1'
+                    carry -= 1
+                
+                else:
+                    res += '0'
+            
+            elif a[i] == '1' and b[i] == '0':
+                if carry > 0:
+                    res += '0'
+                    
+                else:
+                    res += '1'
+            
+            elif a[i] == '0' and b[i] == '1':
+                if carry > 0:
+                    res += '0'
+                
+                else:
+                    res += '1'
+            
+            elif a[i] == '1' and b[i] == '1':
+                if carry > 0:
+                    res += '1'
+                
+                else:
+                    res += '0'
+                    carry += 1
+                
+        if carry == 1:
+            res += str(carry)
+        
+        return res[::-1]
