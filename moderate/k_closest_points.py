4e954e8740b9cbe4cb8a4feb7a14afb0c29851d7
Jinho Lee <jl5027@columbia.edu>
2021-09-20 18:52:17 +0900

    Update k_closest_points.py

--- moderate/k_closest_points.py
+++ moderate/k_closest_points.py
@@ -1,1 +1,10 @@
-
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        distances = [0 for _ in range(len(points))]
+        
+        for idx, point in enumerate(points):
+            distances[idx] = [math.sqrt((point[0]) ** 2 + (point[1]) ** 2), idx]
+            
+        idx = heapq.nsmallest(k, distances, key=lambda i:i[0])
+        
+        return [points[i] for _, i in idx]
