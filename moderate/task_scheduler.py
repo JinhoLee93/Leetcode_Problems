99656fa6e55960fcff6b5544dee0624de22c4329
Jinho Lee <jl5027@columbia.edu>
2021-09-22 22:25:55 +0900

    Create task_scheduler.py

---
+++ moderate/task_scheduler.py
@@ -0,0 +1,30 @@
+class Solution:
+    def leastInterval(self, tasks: List[str], n: int) -> int:
+        count, heap = Counter(tasks), []
+        res = 0
+        
+        for key, val in count.items():
+            heapq.heappush(heap, (-1 * val, key)) # Be aware of the order of the components of the heaps!!!!!!!!
+            
+        while heap: 
+            restack = []
+            cooldown = n
+            first = heapq.heappop(heap)
+            res += 1
+            
+            if first[0] + 1 < 0:
+                restack.append((first[0] + 1, first[1]))
+            
+            while cooldown > 0 and len(restack) > 0:
+                if heap: 
+                    item = heapq.heappop(heap)
+                    if item[0] + 1 < 0:
+                        restack.append((item[0] + 1, item[1]))
+                    
+                res += 1
+                cooldown -= 1
+                
+            for i in range(len(restack)):
+                heapq.heappush(heap, restack[i])
+                
+        return res 
