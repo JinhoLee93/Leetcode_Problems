65a5ca8290b5f17817a793cfc8ed01c83ef906e5
Jinho Lee <jl5027@columbia.edu>
2021-09-24 20:00:30 +0900

    Update check_completeness.py

--- moderate/check_completeness.py
+++ moderate/check_completeness.py
@@ -1,1 +1,29 @@
-
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isCompleteTree(self, root: Optional[TreeNode]) -> bool:
+        res = True
+        
+        def dfs(root):
+            nonlocal res
+            
+            if not root:
+                
+                return (0, 0)
+            
+            ll, lr = dfs(root.left)
+            rl, rr = dfs(root.right)
+            l, r = max(ll, lr), min(rl, rr)
+            
+            if not (rr <= rl <= lr <= ll and 0 <= ll - lr <= 1):
+                res = False
+                
+            return (l + 1, r + 1)
+        
+        l, r = dfs(root)
+        
+        return res and (0 <= l - r <= 1)
