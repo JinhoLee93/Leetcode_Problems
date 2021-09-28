f6dacfe6805e2eb57bd8581bf5f0cc2503c8044f
Jinho Lee <jl5027@columbia.edu>
2021-09-28 18:13:18 +0900

    Update minesweeper.py

--- moderate/minesweeper.py
+++ moderate/minesweeper.py
@@ -1,1 +1,39 @@
+class Solution:
+    def updateBoard(self, board: List[List[str]], click: List[int]) -> List[List[str]]:
+        if board[click[0]][click[1]] == 'M':
+            board[click[0]][click[1]] = 'X'
+            
+            return board
+            
+        ROWS, COLS = len(board), len(board[0])
+        visited = set()
+        moves = [(1, 0), (-1, 0), (0, 1), (0, -1), (1, 1), (-1, -1), (1, -1), (-1, 1)]
+        
+        def dfs(r, c):
+            if board[r][c] != 'E' or \
+                (r, c) in visited:
+                
+                return
 
+            visited.add((r, c))
+            mines = 0
+            for move in moves:
+                nxtR, nxtC = r + move[0], c + move[1]
+                if 0 <= nxtR < ROWS and 0 <= nxtC < COLS and board[nxtR][nxtC] == 'M':
+                    mines += 1
+                    
+            if mines == 0:
+                board[r][c] = 'B'
+            
+            else:
+                board[r][c] =  str(mines)
+                
+            if not board[r][c].isdigit():
+                for move in moves:
+                    nxtR, nxtC = r + move[0], c + move[1]
+                    if 0 <= nxtR < ROWS and 0 <= nxtC < COLS:
+                        dfs(nxtR, nxtC)
+            
+        dfs(click[0], click[1])
+                
+        return board
