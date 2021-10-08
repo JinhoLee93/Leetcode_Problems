7b52006f7918147deebfb6972499f43cfed9f033
Jinho Lee <jl5027@columbia.edu>
2021-10-08 21:50:15 +0900

    Create nested_list_weight_sum.py

---
+++ moderate/nested_list_weight_sum.py
@@ -0,0 +1,63 @@
+# """
+# This is the interface that allows for creating nested lists.
+# You should not implement it, or speculate about its implementation
+# """
+#class NestedInteger:
+#    def __init__(self, value=None):
+#        """
+#        If value is not specified, initializes an empty list.
+#        Otherwise initializes a single integer equal to value.
+#        """
+#
+#    def isInteger(self):
+#        """
+#        @return True if this NestedInteger holds a single integer, rather than a nested list.
+#        :rtype bool
+#        """
+#
+#    def add(self, elem):
+#        """
+#        Set this NestedInteger to hold a nested list and adds a nested integer elem to it.
+#        :rtype void
+#        """
+#
+#    def setInteger(self, value):
+#        """
+#        Set this NestedInteger to hold a single integer equal to value.
+#        :rtype void
+#        """
+#
+#    def getInteger(self):
+#        """
+#        @return the single integer that this NestedInteger holds, if it holds a single integer
+#        Return None if this NestedInteger holds a nested list
+#        :rtype int
+#        """
+#
+#    def getList(self):
+#        """
+#        @return the nested list that this NestedInteger holds, if it holds a nested list
+#        Return None if this NestedInteger holds a single integer
+#        :rtype List[NestedInteger]
+#        """
+
+class Solution:
+    def depthSum(self, nestedList: List[NestedInteger]) -> int:
+        stack = [(nest, 1) for nest in nestedList]
+        combined = []
+        
+        while stack:
+            nest, depth = stack.pop()
+            
+            if nest.isInteger():
+                combined.append((nest.getInteger(), depth))
+            
+            else:
+                for elem in nest.getList():
+                    stack.append((elem, depth + 1))
+                    
+        count = 0
+        for val, dep in combined:
+            count += val * dep
+            
+        return count
