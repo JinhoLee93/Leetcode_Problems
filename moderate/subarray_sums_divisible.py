40ccefce37549e4d769757269ceb40ec72f537de
Jinho Lee <jl5027@columbia.edu>
2021-10-11 01:25:28 +0900

    Update subarray_sums_divisible.py

--- moderate/subarray_sums_divisible.py
+++ moderate/subarray_sums_divisible.py
@@ -1,1 +1,18 @@
-
+class Solution:
+    def subarraysDivByK(self, nums: List[int], k: int) -> int:
+        cur_sum, count = 0, 0
+        sums = defaultdict(int)
+        
+        for num in nums:
+            cur_sum += num 
+            target = cur_sum % k
+            
+            if target == 0:
+                count += 1
+                
+            if target in sums:
+                count += sums[target]
+            
+            sums[target] += 1
+        
+        return count
