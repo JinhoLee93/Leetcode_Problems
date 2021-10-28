090e0a52364fc77b64134e33a99938947f0e24c0
Jinho Lee <jl5027@columbia.edu>
2021-10-28 19:28:15 +0900

    Update perfect_squares.py

--- moderate/perfect_squares.py
+++ moderate/perfect_squares.py
@@ -1,1 +1,16 @@
-
+class Solution:
+    def numSquares(self, n: int) -> int:
+        dp = [float("inf")] * (n + 1)
+        dp[0] = 0
+        
+        for i in range(1, n + 1):
+            for j in range(1, i + 1):
+                square = j ** 2 
+                
+                if i - square < 0:
+                    break
+                
+                if dp[i] > dp[i - square] + 1:
+                    dp[i] = dp[i - square] + 1
+                    
+        return dp[n]
