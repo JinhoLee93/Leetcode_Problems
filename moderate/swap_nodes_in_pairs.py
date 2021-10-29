734c1dee4d67fc5d19eff0046585424947049b3d
Jinho Lee <jl5027@columbia.edu>
2021-10-29 19:23:11 +0900

    Update swap_nodes_in_pairs.py

--- moderate/swap_nodes_in_pairs.py
+++ moderate/swap_nodes_in_pairs.py
@@ -1,1 +1,23 @@
-
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
+      dummy = ListNode(-1)
+      dummy.next = head
+      prv = dummy
+      
+      while head and head.next:
+        odd = head
+        even = head.next
+        
+        prv.next = even
+        odd.next = even.next 
+        even.next = odd
+        prv = odd
+        
+        head = odd.next 
+        
+      return dummy.next
