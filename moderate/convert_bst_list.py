e27e95713bf95e95b962d5e42d150f22b5cb25e4
Jinho Lee <jl5027@columbia.edu>
2021-10-30 21:11:04 +0900

    Update convert_bst_list.py

--- moderate/convert_bst_list.py
+++ moderate/convert_bst_list.py
@@ -1,1 +1,37 @@
 
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+"""
+
+class Solution:
+    def treeToDoublyList(self, root: 'Node') -> 'Node':
+        if not root:
+            
+            return root
+        
+        last, first = None, None
+        
+        def dfs(root):
+            nonlocal last, first
+            if root:
+                dfs(root.left)
+                if not last:
+                    first = root
+                
+                else:
+                    last.right = root
+                    root.left = last
+                
+                last = root
+                dfs(root.right)
+        
+        dfs(root)
+        last.right = first 
+        first.left = last
+        
+        return first
