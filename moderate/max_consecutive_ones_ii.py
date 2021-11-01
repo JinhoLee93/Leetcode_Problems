ee510a65cbfb73b01b7bf6c359df3c3d8102b54a
Jinho Lee <jl5027@columbia.edu>
2021-11-01 23:53:12 +0900

    Update max_consecutive_ones_ii.py

--- moderate/max_consecutive_ones_ii.py
+++ moderate/max_consecutive_ones_ii.py
@@ -1,1 +1,18 @@
 
+class Solution:
+    def findMaxConsecutiveOnes(self, nums: List[int]) -> int:
+        maximum, l, r, zerolen = 0, 0, 0, 0
+        
+        while r < len(nums):
+            if nums[r] == 0:
+                zerolen += 1
+            
+            while zerolen == 2:
+                if nums[l] == 0:
+                    zerolen -= 1
+                l += 1
+                
+            maximum = max(maximum, r - l + 1)
+            r += 1
+
+        return maximum
