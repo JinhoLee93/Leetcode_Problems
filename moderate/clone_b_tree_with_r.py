8a264f22edb01e1b8938b2c512a44b218a07d696
Jinho Lee <jl5027@columbia.edu>
2021-11-10 17:42:53 +0900

    Update clone_b_tree_with_r.py

--- moderate/clone_b_tree_with_r.py
+++ moderate/clone_b_tree_with_r.py
@@ -1,1 +1,42 @@
+# Definition for Node.
+# class Node:
+#     def __init__(self, val=0, left=None, right=None, random=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+#         self.random = random
 
+class Solution:
+    def copyRandomBinaryTree(self, root: 'Node') -> 'NodeCopy':
+        # Do it through two passes
+        if not root:
+            
+            return root
+        
+        graph = defaultdict(NodeCopy)
+        
+        def dfs(root):
+            if root:
+                graph[root] = NodeCopy(root.val)
+                
+                if root.left:
+                    graph[root].left = dfs(root.left)
+                    
+                if root.right:
+                    graph[root].right = dfs(root.right)
+                
+                return graph[root]
+        
+        def link(root):
+            if root:
+                if root.random:
+                    graph[root].random = graph[root.random]
+                    
+                link(root.left)
+                link(root.right)
+            
+        dfs(root)
+        link(root) 
+        
+        return graph[root]
+            
