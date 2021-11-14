2ec98968883a5f0d780f0ebb424cf41a0b36b4c0
Jinho Lee <jl5027@columbia.edu>
2021-11-14 21:04:49 +0900

    Update next_permutation.py

--- moderate/next_permutation.py
+++ moderate/next_permutation.py
@@ -1,1 +1,27 @@
-
+class Solution:
+  def nextPermutation(self, nums):
+    N = len(nums)
+    
+    if N == 1:
+      
+      return
+    
+    pivot = 0
+    for i in range(N - 1, -1, -1):
+      if nums[i] > nums[i - 1]:
+        pivot = i
+        break
+    
+    if pivot == 0:
+      nums.sort()
+      
+      return
+    
+    swap = N - 1
+    while nums[swap] <= nums[pivot - 1]:
+      swap -= 1
+      
+    nums[swap], nums[pivot - 1] = nums[pivot - 1], nums[swap]
+    nums[pivot:] = sorted(nums[pivot:])
+    
+    return
