3bb61407dadd7974c8c0840acdc45f3372c0f86c
Jinho Lee <jl5027@columbia.edu>
2021-11-22 22:41:36 +0900

    Update combination_sum.py

--- moderate/combination_sum.py
+++ moderate/combination_sum.py
@@ -1,1 +1,24 @@
-
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        res = []
+        candidates.sort()
+        
+        def backtrack(i, sub, target):
+            if target == 0:
+                res.append(sub.copy())
+                
+                return
+            
+            elif target < 0:
+                
+                return
+            
+            for j in range(i, len(candidates)):
+                sub.append(candidates[j])
+                backtrack(j, sub, target - candidates[j])
+                sub.pop()
+        
+        backtrack(0, [], target)
+        
+        return res
+                
