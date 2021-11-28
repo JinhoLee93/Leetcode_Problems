9102295a5f2455d2420cf98db6013dc49c4ab0b7
Jinho Lee <jl5027@columbia.edu>
2021-11-28 21:01:41 +0900

    Update expressive_words.py

--- moderate/expressive_words.py
+++ moderate/expressive_words.py
@@ -1,35 +1,48 @@
 class Solution:
     def expressiveWords(self, s: str, words: List[str]) -> int:
-        def group_words(word: str) -> list[tuple]:
-            group = []
-            i = 0
-            while i < len(word):
-                char = word[i]
-                start = i
-                while i < len(word) and word[i] == char:
-                    i += 1
-                group.append((char, i-start))
-
-            return group
-        
-        def check_stretchy(s_group: list[tuple], word_group: list[tuple]) -> bool:
-            if len(s_group) != len(word_group):
-                
-                return False
+        def union(s: str) -> List[tuple]:
+            uni = []
+            count = 1
+            char = ""
             
-            for i in range(len(s_group)):
-                if s_group[i][0] != word_group[i][0] or s_group[i][1] < word_group[i][1] or (s_group[i][1] > word_group[i][1] and s_group[i][1] < 3):
+            for i in range(len(s)):
+                if char != s[i]:
+                    if char == "":
+                        char = s[i]
+                        continue
+                    
+                    else:
+                        uni.append((char, count))
+                        char = s[i]
+                        count = 1
+                
+                else:
+                    count += 1
+            
+            uni.append((char, count))
+                
+            return uni
+        
+        def check(sSub: List[tuple], wSub: List[tuple]) -> bool:
+            if len(sSub) != len(wSub):
+                
+                return False 
+            
+            for i in range(len(sSub)):
+                if sSub[i][0] != wSub[i][0] or (sSub[i][1] < 3 and sSub[i][1] != wSub[i][1]) or \
+                    sSub[i][1] < wSub[i][1]:
                     
                     return False
                 
             return True
         
-        ans = 0
-        s_group = group_words(s)
+        sSub = union(s)
+        res = 0
         
         for word in words:
-            word_group = group_words(word)
-            if check_stretchy(s_group, word_group):
-                ans += 1
-                
-        return ans
+            wSub = union(word)
+            
+            if check(sSub, wSub):
+                res += 1
+
+        return res
