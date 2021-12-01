fa6952b1657fd681e105633bcf6d7ec93ee5b9e5
Jinho Lee <jl5027@columbia.edu>
2021-12-01 23:39:05 +0900

    Update getrandom.py

--- moderate/getrandom.py
+++ moderate/getrandom.py
@@ -1,1 +1,34 @@
+class RandomizedSet:
 
+    def __init__(self):
+        self.rand = set()
+
+    def insert(self, val: int) -> bool:
+        if val in self.rand:
+            
+            return False
+        
+        self.rand.add(val)
+        
+        return True
+
+    def remove(self, val: int) -> bool:
+        if val not in self.rand:
+            
+            return False
+        
+        self.rand.remove(val)
+        
+        return True
+        
+
+    def getRandom(self) -> int:
+        
+        return random.choice(tuple(self.rand))
+    
+
+# Your RandomizedSet object will be instantiated and called as such:
+# obj = RandomizedSet()
+# param_1 = obj.insert(val)
+# param_2 = obj.remove(val)
+# param_3 = obj.getRandom()
