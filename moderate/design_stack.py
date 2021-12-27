a1781683f4aa7229bd74ba26fd206faa4d4c25f0
Jinho Lee <jl5027@columbia.edu>
2021-12-27 23:32:53 +0900

    Update design_stack.py

--- moderate/design_stack.py
+++ moderate/design_stack.py
@@ -1,1 +1,30 @@
+class CustomStack:
 
+    def __init__(self, maxSize: int):
+        self.stack = []
+        self.maxSize = maxSize
+
+    def push(self, x: int) -> None:
+        if len(self.stack) < self.maxSize:
+            self.stack.append(x)
+
+    def pop(self) -> int:
+        
+        return self.stack.pop() if self.stack else -1
+
+    def increment(self, k: int, val: int) -> None:
+        if len(self.stack) < k:
+            for i in range(len(self.stack)):
+                self.stack[i] += val
+        
+        else:
+            for i in range(k):
+                self.stack[i] += val
+        
+
+
+# Your CustomStack object will be instantiated and called as such:
+# obj = CustomStack(maxSize)
+# obj.push(x)
+# param_2 = obj.pop()
+# obj.increment(k,val)
